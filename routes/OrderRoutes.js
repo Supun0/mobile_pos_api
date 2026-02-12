b90debc5a8e0042dffae7994c3c6cce2b58c3fcc
Supun0 <supunnncabe@gmail.com>
2026-02-12 15:52:53 +0530

    10

---
+++ routes/OrderRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const {getOrders, getOrder, createOrder, updateOrder, deleteOrder} = require("../controllers/orderController");
+const {protect} = require('../middleware/auth');
+
+router.route('/')
+  .get(protect, getOrders)
+  .post(protect, createOrder  );
+
+router.route('/:id')
+  .get(protect, getOrder)
+  .put(protect, updateOrder)
+  .delete(protect, deleteOrder);
+
+module.exports = router;
