da715da63227cd4435821f79c7a856d91bc99c9b
Supun0 <supunnncabe@gmail.com>
2026-02-12 11:42:44 +0530

    4

---
+++ models/Order.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+      required: [true, "Please add a customer"],
+    },
+    date:{
+      type: Date,
+      default: Date.now
+    },
+    totalAmount:{
+      type: Number,
+      required: [true, "Please add a total amount"],
+      min: 0
+    },
+    productDetails:[{
+      product:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+      },
+      quantity:{
+        type: Number,
+        required:true,
+        min: 1
+      },
+      price:{
+        type: Number,
+        required:true,
+        min: 0
+      }
+    }]
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Order", orderSchema);
